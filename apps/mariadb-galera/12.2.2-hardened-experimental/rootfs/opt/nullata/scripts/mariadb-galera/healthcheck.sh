3769d60cfac438d9baa951df1183d05b497b5d8a
nullata <niuzunov@hotmail.com>
2026-03-09 16:15:53 +0200

    add build 12.2.2-hardened-experimental

---
+++ apps/mariadb-galera/12.2.2-hardened-experimental/rootfs/opt/nullata/scripts/mariadb-galera/healthcheck.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright Broadcom, Inc. All Rights Reserved.
+# SPDX-License-Identifier: APACHE-2.0
+# Modified by nullata 2025
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+# Load libraries
+. /opt/nullata/scripts/libmariadbgalera.sh
+
+# Load MariaDB environment variables
+. /opt/nullata/scripts/mariadb-env.sh
+
+mysql_healthcheck
